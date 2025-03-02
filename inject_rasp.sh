e21da2a919cdedbb4d1fad2bd103ecb9b2d0bf02
michelleamesquita <44561732+michelleamesquita@users.noreply.github.com>
2025-03-02 16:30:33 -0300

    Update inject_rasp.sh

--- inject_rasp.sh
+++ inject_rasp.sh
@@ -8,17 +8,30 @@ if [ -f "$MAIN_ACTIVITY" ]; then
     echo "Modificando MainActivity.java para incluir RASP..."
 
     # Adiciona importações do Android RASP e Toast
-    sed -i '/import android.os.Bundle;/a import securevale.rasp.RASP;\nimport android.widget.Toast;' $MAIN_ACTIVITY
+    sed -i '/import android.os.Bundle;/a \
+    import com.securevale.rasp.android.api.SecureAppChecker;\n\
+    import com.securevale.rasp.android.api.result.Result;\n\
+    import android.widget.Toast;' $MAIN_ACTIVITY
 
-    # Adiciona código dentro do onCreate para verificar root e emulador
+    # Adiciona inicialização do SecureApp dentro do onCreate
     sed -i '/setContentView(R.layout.activity_main);/a \
-        \n        // Inicializa o RASP\n        RASP rasp = new RASP(this);\n\
-        if (rasp.isRooted()) {\n\
-            Toast.makeText(this, "Dispositivo rooteado! Encerrando o app.", Toast.LENGTH_LONG).show();\n\
+        \n        SecureApp.init();\n\
+        \n        boolean shouldCheckForEmulator = true;\n\
+        boolean shouldCheckForDebugger = true;\n\
+        boolean shouldCheckForRoot = true;\n\
+        \n        SecureAppChecker.Builder builder = new SecureAppChecker.Builder(\n\
+            this, shouldCheckForEmulator, shouldCheckForDebugger, shouldCheckForRoot\n\
+        );\n\
+        \n        SecureAppChecker check = builder.build();\n\
+        Result checkResult = check.check();\n\
+        \n        if (checkResult instanceof Result.EmulatorFound) {\n\
+            Toast.makeText(this, \"App está rodando em um emulador!\", Toast.LENGTH_LONG).show();\n\
             finish();\n\
-        }\n\
-        if (rasp.isEmulator()) {\n\
-            Toast.makeText(this, "Emulador detectado! Encerrando o app.", Toast.LENGTH_LONG).show();\n\
+        } else if (checkResult instanceof Result.DebuggerEnabled) {\n\
+            Toast.makeText(this, \"App está em modo de depuração!\", Toast.LENGTH_LONG).show();\n\
+            finish();\n\
+        } else if (checkResult instanceof Result.Rooted) {\n\
+            Toast.makeText(this, \"Dispositivo rooteado!\", Toast.LENGTH_LONG).show();\n\
             finish();\n\
         }' $MAIN_ACTIVITY
 
